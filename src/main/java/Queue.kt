694dafdaedd87f7bd2dc9e93fe14202efdee89aa
dong <dongorigin@gmail.com>
2018-05-06 17:51:09 +0800

    new: Queue

---
+++ src/main/java/Queue.kt
@@ -0,0 +1,41 @@
+/**
+ * @author dong on 2018/05/06.
+ */
+class Queue<Item> : Iterable<Item> {
+    private var first: Node<Item>? = null
+    private var last: Node<Item>? = null
+    private var size = 0
+
+    fun enqueue(item: Item) {
+        val new = Node(item)
+        if (last == null) {
+            first = new
+            last = new
+        } else {
+            last?.next = new
+            last = new
+        }
+        size++
+    }
+
+    fun dequeue(): Item {
+        first?.let {
+            first = it.next
+            if (first == null) {
+                last = null
+            }
+            size--
+            return it.item
+        } ?: throw NoSuchElementException()
+    }
+
+    fun isEmpty(): Boolean {
+        return first == null
+    }
+
+    fun size() = size
+
+    override fun iterator(): Iterator<Item> {
+        return NodeIterator(first)
+    }
+}
---
+++ src/test/java/QueueTest.kt
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.StdOut
+import org.junit.Test
+
+/**
+ * @author dong on 2018/05/06.
+ */
+class QueueTest {
+    private val source = arrayOf("to", "be", "or", "not", "to", "-", "be", "-", "-", "that", "-", "-", "-", "is")
+
+    @Test
+    fun test() {
+        val queue = Queue<String>()
+        source.forEach {
+            when {
+                it != "-" -> queue.enqueue(it)
+                !queue.isEmpty() -> StdOut.print(queue.dequeue() + " ")
+            }
+        }
+        StdOut.print("(${queue.size()} left on queue)")
+
+        StdOut.print("\nqueue: ")
+        for (s in queue) {
+            StdOut.print(s + " ")
+        }
+    }
+}
