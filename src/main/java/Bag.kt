47ea42967a76426f63193f6119c09a974f41f7f1
dong <dongorigin@gmail.com>
2018-05-06 17:59:07 +0800

    new: Bag

---
+++ src/main/java/Bag.kt
@@ -0,0 +1,27 @@
+/**
+ * @author dong on 2018/05/06.
+ */
+class Bag<Item> : Iterable<Item> {
+    private var first: Node<Item>? = null
+    private var size = 0
+
+    fun add(item: Item) {
+        val oldFirst = first
+        first = Node(item, oldFirst)
+        size++
+    }
+
+
+    fun isEmpty(): Boolean {
+        return first == null
+    }
+
+    fun size(): Int {
+        return size
+    }
+
+    override fun iterator(): Iterator<Item> {
+        return NodeIterator(first)
+    }
+
+}
---
+++ src/test/java/BagTest.kt
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdOut
+import org.junit.Test
+
+/**
+ * @author dong on 2018/05/06.
+ */
+class BagTest {
+    private val source = arrayOf("to", "be", "or", "not", "to", "-", "be", "-", "-", "that", "-", "-", "-", "is")
+
+    @Test
+    fun test() {
+        val bag = Bag<String>()
+        source.forEach {
+            bag.add(it)
+        }
+
+        StdOut.print("bag: ")
+        bag.forEach { StdOut.print(it + " ") }
+    }
+}
