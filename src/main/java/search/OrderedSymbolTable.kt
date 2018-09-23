41eea32a81349308f6706292760b0fd2108fe231
dong <dongorigin@gmail.com>
2018-09-23 17:59:40 +0800

    new: sequential search symbol table

---
+++ src/main/java/search/OrderedSymbolTable.kt
@@ -0,0 +1,17 @@
+package search
+
+/**
+ * @author dong on 2018/09/22.
+ */
+interface OrderedSymbolTable<Key : Comparable<Key>, Value> : SymbolTable<Key, Value> {
+    fun min(): Key
+    fun max(): Key
+    fun floor(): Key
+    fun ceiling(): Key
+    fun rank(key: Key): Int
+    fun select(rank: Int): Key
+    fun deleteMin()
+    fun deleteMax()
+    fun size(low: Key, high: Key): Int
+    fun keys(low: Key, high: Key): Iterable<Key>
+}
